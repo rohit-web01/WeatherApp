e994d056459b4cb69d1d1ff7a7b0733a934485af
Rohit Gehlot <rohitgehlot5621@gmail.com>
2024-12-07 19:56:18 +0530

    Adding Essential Files & Completing It's Style Part

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: ["*"], // Specify where your HTML files are located
+    theme: {
+      extend: {
+        color:{
+          'custom-color': '#123456',
+        }
+      },
+    },
+  };
+  
